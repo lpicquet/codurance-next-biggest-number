54d917417b30798c932a4210f1bdf891d14edcc5
laurent.picquet <lpicquet@gmail.com>
2019-07-15 20:12:26 +0100

    WIP: trying some things, but need to leave!!!

--- src/main/java/next/big/thing/NextBigThingCalculator.java
+++ src/main/java/next/big/thing/NextBigThingCalculator.java
@@ -1,7 +1,31 @@
 package next.big.thing;
 
 public class NextBigThingCalculator {
-  public int next(int aSingleDigitNumber) {
+  public int next(int aNumber) {
+    String[] digits = String.valueOf(aNumber).split("");
+
+    if (digits.length == 1){
+      return -1;
+    }
+
+    int firstDigit = Integer.valueOf(digits[0]);
+    // remove the first digit from the list of digit
+    findHighestNumber(digits);
+    findTheLowestNumber(digits);
+    int nextDigit = findNextBigestDigit(digits, firstDigit);
+
+
+  }
+
+  private int findNextBigestDigit(String[] digits, int firstDigit) {
+    Integer nextBiggestDigit = null;
+
+    for (int i = 0; i < digits.length; i++) {
+      int aDigit = Integer.valueOf(digits[i]);
+      if (nextBiggestDigit == null || aDigit < nextBiggestDigit || aDigit > firstDigit) {
+          nextBiggestDigit = aDigit;
+        }
+    }
     return 0;
   }
 }
--- src/test/java/next/big/thing/TestTheThing.java
+++ src/test/java/next/big/thing/TestTheThing.java
@@ -5,20 +5,44 @@ import org.junit.Test;
 
 public class TestTheThing {
 
+  NextBigThingCalculator nextBigThingCalculator = new NextBigThingCalculator();
+
   @Test
   public void testItGuessesTheNextNumberGivenAsingleDigitNumber(){
+
+
+
+
+    assertNextBigThing(1, -1);
+
+  }
+
+  @Test
+  public void testItGuessesTheNextNumberGivenATwoDigitNumberLower(){
+    assertNextBigThing(10, -1);
+
+  }
+
+  @Test
+  public void testATwoDigitNumberWithIdenticalDigits(){
+    assertNextBigThing(11, -1);
+  }
+
+  @Test
+  public void testATwoDigitNumberLower(){
+    assertNextBigThing(12, 21);
+  }
+
+  private void assertNextBigThing(int aNumber, int expected) {
+    int actual = nextBigThingCalculator.next(aNumber);
+
 
     // given a number is a single digit number
     // when I compute the next big then
     // then I should be given -1
 
-    int aSingleDigitNumber = 1;
-    NextBigThingCalculator nextBigThingCalculator = new NextBigThingCalculator();
-
-    int actual = nextBigThingCalculator.next(aSingleDigitNumber);
-
-    Assert.assertEquals("the next big thing is not as expected", -1, actual);
-
+    Assert.assertEquals("the next big thing is not as expected", expected, actual);
   }
+
 
 }
